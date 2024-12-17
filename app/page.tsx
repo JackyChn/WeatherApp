d763a0ea8b7d014d043abe2837785498ed17dc6a
JackyChn <ccc1020048076@gmail.com>
2024-12-17 20:22:33 +1030

    Update: folder structure; Add: search button

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { getWeatherInfo } from "@/services/weatherApi";
+import Weather from "@/components/ui/Weather";
 import Image from "next/image";
 import React from "react";
 
@@ -13,18 +13,8 @@ export default function HomePage() {
         priority
         className="object-cover z-[-10]"
       />
-      <div className="w-96 h-96 bg-slate-300 bg-opacity-80 border-2 border-colors-secondary rounded-xl">
-        <h1 className="text-center font-semibold mt-2">
-          Check weather for any city
-        </h1>
-        <div className="w-full flex items-center justify-center mt-6">
-          <input
-            type="text"
-            placeholder="Enter city name...."
-            className="w-full max-w-[80%] p-2 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400"
-          />
-        </div>
-      </div>
+      {/* Main Weather box */}
+      <Weather />
     </main>
   );
 }
---
+++ components/ui/SearchButton.tsx
@@ -0,0 +1,15 @@
+"use client";
+import React from "react";
+import { IconClipboard } from "@tabler/icons-react";
+import { Search } from "lucide-react";
+
+export default function SearchButton() {
+  return (
+    <>
+      <IconClipboard className="absolute top-2 right-2 text-neutral-300 group-hover/btn:block hidden h-4 w-4 transition duration-200" />
+      <button className="ml-2 px-2 py-2 rounded-full bg-gradient-to-b from-blue-500 to-blue-600 text-white focus:ring-2 focus:ring-blue-400 hover:shadow-xl transition duration-200">
+        <Search />
+      </button>
+    </>
+  );
+}
