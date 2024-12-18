d5178e1ce9750168735a7c8bba04e8d69df6f448
JackyChn <ccc1020048076@gmail.com>
2024-12-18 22:06:18 +1030

    Fix: Hero responsive position

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,9 +3,9 @@ import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 
 export default function Hero() {
   return (
-    <div className="absolute left-1/2 top-32 z-10 -translate-x-1/2 transform md:top-24">
+    <div className="absolute left-1/2 top-20 z-10 -translate-x-1/2 transform md:top-24">
       <TextGenerateEffect
-        words={"Check weather for any city in the world!"}
+        words={"Check weather for any city!"}
         className="lg:text-4xl"
       />
     </div>
